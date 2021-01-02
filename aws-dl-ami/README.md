07530bc288433d37bdc6db57c10dc7360558b39f
hongjee <hongjee@yahoo.com>
2021-01-01 23:07:33 -0500

    add aws-dl-ami

---
+++ aws-dl-ami/README.md
@@ -0,0 +1,42 @@
+# AWS Deep Learning AMI 
+
+```
+=============================================================================
+       __|  __|_  )
+       _|  (     /   Deep Learning AMI (Amazon Linux) Version 38.0
+      ___|\___|___|
+=============================================================================
+
+Please use one of the following commands to start the required environment with the framework of your choice:
+for AWS MX 1.6 (+Keras2) with Python3 (CUDA 10.1 and Intel MKL-DNN) _______________________________ source activate mxnet_p36
+for AWS MX 1.6 (+Keras2) with Python2 (CUDA 10.1 and Intel MKL-DNN) _______________________________ source activate mxnet_p27
+for AWS MX 1.7 (+Keras2) with Python3 (CUDA 10.1 and Intel MKL-DNN) ________________________ source activate mxnet_latest_p37
+for AWS MX(+Amazon Elastic Inference) with Python3 _______________________________________ source activate amazonei_mxnet_p36
+for AWS MX(+Amazon Elastic Inference) with Python2 _______________________________________ source activate amazonei_mxnet_p27
+for TensorFlow(+Keras2) with Python3 (CUDA 10.0 and Intel MKL-DNN) __________________________ source activate tensorflow_p36
+for TensorFlow(+Keras2) with Python2 (CUDA 10.0 and Intel MKL-DNN) __________________________ source activate tensorflow_p27
+for Tensorflow(+Amazon Elastic Inference) with Python2 _____________________________ source activate amazonei_tensorflow_p27
+for Tensorflow(+Amazon Elastic Inference) with Python3 _____________________________ source activate amazonei_tensorflow_p36
+for TensorFlow 2(+Keras2) with Python3 (CUDA 10.1 and Intel MKL-DNN) _______________________ source activate tensorflow2_p36
+for TensorFlow 2(+Keras2) with Python2 (CUDA 10.1 and Intel MKL-DNN) _______________________ source activate tensorflow2_p27
+for TensorFlow 2.3 with Python3.7 (CUDA 10.2 and Intel MKL-DNN) _____________________ source activate tensorflow2_latest_p37
+for TensorFlow 2(+Amazon Elastic Inference) with Python3 __________________________ source activate amazonei_tensorflow2_p36
+for TensorFlow 2(+Amazon Elastic Inference) with Python2 __________________________ source activate amazonei_tensorflow2_p27
+for PyTorch 1.4 with Python3 (CUDA 10.1 and Intel MKL) _________________________________________ source activate pytorch_p36
+for PyTorch 1.4 with Python2 (CUDA 10.1 and Intel MKL) _________________________________________ source activate pytorch_p27
+for PyTorch 1.7 with Python3 (CUDA 10.1 and Intel MKL) __________________________________ source activate pytorch_latest_p36
+for PyTorch(+Amazon Elastic Inference) with Python3 ___________________________________ source activate amazonei_pytorch_p36
+for Chainer with Python2 (CUDA 10.0 and Intel iDeep) ___________________________________________ source activate chainer_p27
+for Chainer with Python3 (CUDA 10.0 and Intel iDeep) ___________________________________________ source activate chainer_p36
+for base Python2 (CUDA 10.0) _______________________________________________________________________ source activate python2
+for base Python3 (CUDA 10.0) _______________________________________________________________________ source activate python3
+
+To automatically activate base conda environment upon login, run: 'conda config --set auto_activate_base true'
+ 
+Official Conda User Guide: https://docs.conda.io/projects/conda/en/latest/user-guide/
+AWS Deep Learning AMI Homepage: https://aws.amazon.com/machine-learning/amis/
+Developer Guide and Release Notes: https://docs.aws.amazon.com/dlami/latest/devguide/what-is-dlami.html
+Support: https://forums.aws.amazon.com/forum.jspa?forumID=263
+For a fully managed experience, check out Amazon SageMaker at https://aws.amazon.com/sagemaker
+=============================================================================
+```
---
+++ aws-dl-ami/examples/horovod/tensorflow2/train_tf2_resnet.py
@@ -0,0 +1,144 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Reference https://github.com/aws-samples/deep-learning-models/blob/tf2/models/resnet/tensorflow2/train_tf2_resnet.py
+
+
+
+
+import os
+import numpy as np
+import getpass
+import tensorflow as tf
+import horovod.tensorflow as hvd
+from tensorflow.python.util import nest
+import argparse
+from time import time, sleep
+
+@tf.function
+def parse(record):
+    features = {'image/encoded': tf.io.FixedLenFeature((), tf.string),
+                'image/class/label': tf.io.FixedLenFeature((), tf.int64)}
+    parsed = tf.io.parse_single_example(record, features)
+    image = tf.image.decode_jpeg(parsed['image/encoded'])
+    image = tf.image.resize(image, (224, 224))
+    image = tf.cast(image, tf.float16)
+    label = tf.cast(parsed['image/class/label'] - 1, tf.int32)
+    return image, label
+
+def data_gen():
+    input_shape = [224, 224, 3]
+    while True:
+        image = tf.random.uniform(input_shape)
+        label = tf.random.uniform(minval=0, maxval=999, shape=[1], dtype=tf.int32)
+        yield image, label
+
+def create_data(data_dir = None, synthetic=False, batch_size=128):
+    if synthetic:
+        ds = tf.data.Dataset.from_generator(data_gen, output_types=(tf.float32, tf.int32))
+    else:
+        filenames = [os.path.join(data_dir, i) for i in os.listdir(data_dir)]
+        ds = tf.data.TFRecordDataset(filenames).shard(hvd.size(), hvd.rank())
+        ds = ds.map(parse, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    ds = ds.batch(batch_size).prefetch(128)
+    return ds
+
+@tf.function
+def train_step(model, opt, loss_func, images, labels, first_batch, fp32=False):
+    with tf.GradientTape() as tape:
+        probs = model(images, training=True)
+        loss_value = loss_func(labels, probs)
+        if not fp32:
+            scaled_loss = opt.get_scaled_loss(loss_value)
+    tape = hvd.DistributedGradientTape(tape, compression=hvd.Compression.fp16)
+    if fp32:
+        grads = tape.gradient(loss_value, model.trainable_variables)
+    else:
+        scaled_grads = tape.gradient(scaled_loss, model.trainable_variables)
+        grads = opt.get_unscaled_gradients(scaled_grads)
+    opt.apply_gradients(zip(grads, model.trainable_variables))
+    if first_batch:
+        hvd.broadcast_variables(model.variables, root_rank=0)
+        hvd.broadcast_variables(opt.variables(), root_rank=0)
+    return loss_value
+
+def add_cli_args():
+    cmdline = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    cmdline.add_argument('--data_dir', default='',
+                         help="""Path to dataset in TFRecord format
+                             (aka Example protobufs). Files should be
+                             named 'train-*' and 'validation-*'.""")
+    cmdline.add_argument('-b', '--batch_size', default=128, type=int,
+                         help="""Size of each minibatch per GPU""")
+    cmdline.add_argument('--num_batches', default=100, type=int,
+                         help="""Number of batches to run.
+                             Ignored during eval or if num epochs given""")
+    cmdline.add_argument('-lr', '--learning_rate', default=0.01, type=float,
+                         help="""Start learning rate""")
+    cmdline.add_argument('--momentum', default=0.01, type=float,
+                         help="""Start learning rate""")
+    cmdline.add_argument('-fp32', '--fp32',
+                         help="""disable mixed precision training""",
+                         action='store_true')
+    cmdline.add_argument('-xla_off', '--xla_off',
+                         help="""disable xla""",
+                         action='store_true')
+    cmdline.add_argument('-s', '--synthetic',
+                         help="""Use synthetic data for training""",
+                         action='store_true')
+    return cmdline
+
+def main():
+    # setup horovod
+    start = time()
+    hvd.init()
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    if gpus:
+        tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    cmdline = add_cli_args()
+    FLAGS, unknown_args = cmdline.parse_known_args()
+    if not FLAGS.xla_off:
+        tf.config.optimizer.set_jit(True)
+    if not FLAGS.fp32:
+        tf.config.optimizer.set_experimental_options({"auto_mixed_precision": True})
+    ds = create_data(FLAGS.data_dir, FLAGS.synthetic, FLAGS.batch_size)
+    model = tf.keras.applications.ResNet50(weights=None, classes=1000)
+    opt = tf.keras.optimizers.SGD(learning_rate=FLAGS.learning_rate * hvd.size(), momentum=0.1)
+    if not FLAGS.fp32:
+        opt = tf.keras.mixed_precision.experimental.LossScaleOptimizer(opt, loss_scale="dynamic")
+    loss_func = tf.keras.losses.SparseCategoricalCrossentropy()
+
+    loop_time = time()
+    if hvd.local_rank() == 0:
+        print("Step \t Throughput \t Loss")
+    for batch, (images, labels) in enumerate(ds):
+        loss = train_step(model, opt, loss_func, images, labels, batch==0, fp32=FLAGS.fp32)
+        if hvd.local_rank() == 0:
+            duration = time() - loop_time
+            loop_time = time()
+            throughput = (hvd.size()*FLAGS.batch_size)/duration
+            print("{} \t images/sec: {} \t {}".format(batch, throughput, loss))
+        if batch==FLAGS.num_batches:
+            break
+    if hvd.rank() == 0:
+        print("\nFinished in {}".format(time()-start))
+
+if __name__=='__main__':
+    main()
